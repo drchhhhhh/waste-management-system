0f201d5eb932d6f75d481559f2f787441fd06323
drich <dricharenas@gmail.com>
2026-05-09 00:58:57 +0800

    fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,18 @@
+// src/firebase/config.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCup0g6iyzY5wgacCou98FWsW-WaTjJFes",
-  authDomain: "waste-management-system-3f142.firebaseapp.com",
-  projectId: "waste-management-system-3f142",
-  storageBucket: "waste-management-system-3f142.firebasestorage.app",
-  messagingSenderId: "590728672571",
-  appId: "1:590728672571:web:907e068be87c03ed3a731e"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { db };
